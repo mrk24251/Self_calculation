17dcbc8211796657240c90ded292f676cbdc4c4c
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2020-08-15 19:37:05 +0430

    change databse_url to heroku url

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"postgresql+psycopg2cffi://user:password@host:port/dbname[?key=value&key=value...]
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://punish:punish@127.0.0.1:5432"
+SQLALCHEMY_DATABASE_URL = "postgres://ecotulybegudqp:ffbc23c3e2a96e020abd94c79d7382774ca79e119dffc871ee932184b5a6d47e@ec2-107-22-7-9.compute-1.amazonaws.com:5432/dvvi34oj0bas5"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
