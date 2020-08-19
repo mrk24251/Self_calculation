ecac018751b41583ae016d6c4f481454df2c3127
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2020-08-19 16:25:11 +0430

    replace psycop by psycop-binary

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# postgresql+psycopg2://punish:punish@127.0.0.1:5432
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"postgresql+psycopg2cffi://user:password@host:port/dbname[?key=value&key=value...]
-SQLALCHEMY_DATABASE_URL = "postgres://ecotulybegudqp:ffbc23c3e2a96e020abd94c79d7382774ca79e119dffc871ee932184b5a6d47e@ec2-107-22-7-9.compute-1.amazonaws.com:5432/dvvi34oj0bas5"
+SQLALCHEMY_DATABASE_URL = "postgres://edivuszmtozaer:5cd123b632aceca4937d0a3cebf94efff795f6ac831819a8f5e70d619b4c4c11@ec2-34-238-26-109.compute-1.amazonaws.com:5432/ddl7hva590ggcq"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
