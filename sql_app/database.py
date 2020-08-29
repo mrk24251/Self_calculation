20dd8bbc59f171ba99066dd984e7945377c75754
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2020-08-29 10:15:27 +0430

    exam2

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,10 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+import psycopg2
 
 # postgresql+psycopg2://punish:punish@127.0.0.1:5432
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"postgresql+psycopg2cffi://user:password@host:port/dbname[?key=value&key=value...]
 SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+
+conn = psycopg2.connect(SQLALCHEMY_DATABASE_URL, sslmode='require')
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
