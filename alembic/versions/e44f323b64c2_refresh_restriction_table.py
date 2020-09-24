07a1e7ccb2c0df9b776cda54c950d5c7152e5ac8
mohammadreza karami <mohammadreza.karami22@yahoo.com>
2020-09-24 16:39:42 +0330

    refresh restriction table

--- alembic/versions/e44f323b64c2_refresh_restriction_table.py
+++ alembic/versions/e44f323b64c2_refresh_restriction_table.py
@@ -1,8 +1,8 @@
-"""create restriction table
+"""refresh restriction table
 
-Revision ID: d7f2744a91d8
-Revises: 28332ddf91d4
-Create Date: 2020-09-22 10:49:09.960990
+Revision ID: e44f323b64c2
+Revises: d7f2744a91d8
+Create Date: 2020-09-24 16:38:02.097314
 
 """
 from alembic import op
@@ -10,20 +10,20 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'd7f2744a91d8'
-down_revision = '28332ddf91d4'
+revision = 'e44f323b64c2'
+down_revision = 'd7f2744a91d8'
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-
+    op.drop_table("restriction")
     op.create_table(
         "restriction",
         sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("reason",sa.String(100)),
-        sa.Column("endingWith", sa.String(100)),
-        sa.Column("myRestriction", sa.String(100)),
+        sa.Column("reason",sa.String(300)),
+        sa.Column("endingWith", sa.String(500)),
+        sa.Column("myRestriction", sa.String(300)),
         sa.Column("created_date", sa.Date),
     )
 
